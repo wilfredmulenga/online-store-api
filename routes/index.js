da28e8b4a98df8f372a4bbec864abb4ed398ffd5
wilfred <wilfred.zazuafrica.com>
2022-08-24 02:51:17 +0200

    add product

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 const router = express.Router()
 
+import productRouter from './product/index.js'
+import productsRouter from './products/index.js'
 import userRouter from './user/index.js'
 import usersRouter from './users/index.js'
-import productsRouter from './products/index.js'
 
+router.use('/product', productRouter)
+router.use('/products', productsRouter)
 router.use('/user', userRouter)
 router.use('/users', usersRouter)
-router.use('/products', productsRouter)
 
 export default router
