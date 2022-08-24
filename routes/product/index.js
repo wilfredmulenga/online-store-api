c555d7f62963b4c43e9bca7449619cf0d7cf4476
wilfred <wilfred.zazuafrica.com>
2022-08-24 03:06:01 +0200

    update and delete product

--- routes/product/index.js
+++ routes/product/index.js
@@ -28,4 +28,42 @@ router.post('/', function (req, res) {
     res.sendStatus(200)
 })
 
+router.patch('/', function (req, res) {
+    try {
+        const { category, description, image, price, title, productKey } =
+            req.body
+
+        // TODO: check if key exists in database and get the record
+        ref.update({
+            [productKey]: {
+                category,
+                description,
+                id: [productKey],
+                image,
+                price,
+                title,
+                rating: {
+                    count: 0,
+                    rate: 0,
+                },
+            },
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+    res.sendStatus(200)
+})
+
+router.delete('/', function (req, res) {
+    try {
+        const { productKey } = req.body
+        ref.child(productKey).set(null)
+    } catch (error) {
+        console.log(error)
+    }
+
+    res.sendStatus(200)
+})
+
 export default router
